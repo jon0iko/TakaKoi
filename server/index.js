581b0e5207e8982c7336622918b0c307b48fce85
jon0iko <price6967@gmail.com>
2023-07-05 11:43:41 +0600

    created server folder for api deployment

--- server/index.js
+++ server/index.js
@@ -9,9 +9,9 @@ const PORT = process.env.PORT || 5000;
 //middleware.
 app.use(cors());
 app.use(express.json()); // for req.body
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "client/build")));
-}
+// if(process.env.NODE_ENV === "production"){
+//   app.use(express.static(path.join(__dirname, "client/build")));
+// }
 
 //ROUTES//
 
@@ -87,7 +87,7 @@ app.delete("/expense/:id", async (req, res) => {
 });
 
 app.get("*", (req,res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+  res.send("This is not the page you are looking for");
 });
 
 app.listen(PORT, () => {
